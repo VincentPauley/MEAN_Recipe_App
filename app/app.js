91f194da8a4a6776ca2d2b6651a65b9eed1c833a
Vincent P <vinniepauley@gmail.com>
2016-12-03 11:53:18 -0600

    sample find all recipes

--- app/app.js
+++ app/app.js
@@ -1,1 +1,26 @@
-ECHO is on.
+var app = angular.module('app', []);
+
+
+app.controller('showAllRecipes', function() {
+    this.test = "Lucy";
+    
+    var recipes = [
+        {
+            recipe_name : "Chilli",
+            meal_type : "Lunch/Dinner"
+        },
+        {
+            recipe_name : "Omlette",
+            meal_type : "Breakfast"
+        },
+        {
+            recipe_name : "Grilled Cheese",
+            meal_type : "Lunch"
+        },
+        {
+            recipe_name : "Chicken Parmeasean",
+            meal_type : "Dinner"
+        }
+    ];
+    this.recipe_list = recipes;
+});
